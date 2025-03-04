d0270c276a47d04848f71f5130fab32f00036ea5
Codiemaureen <codiestover.cs@gmail.com>
2025-03-04 16:45:40 -0500

    Fixing api calls to display the cards

--- js/main.js
+++ js/main.js
@@ -1,33 +1,42 @@
-let deckId = '';
-
-!localStorage.getItem(deckId) ? getDeckID : '';
-
 document.querySelector('.draw').addEventListener('click', drawTwo);
 
-function getDeckID() {
-  fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    .then(res =>  res.json())
-    .then(data => {
-      console.log('id');
-      let deckId = localStorage.setItem('deckId', data.deckId);
-      return deckId;
-    })
-    .catch((err) => {
-      console.log(`error ${err}` )
-    })
-};
 
-function drawTwo(event){
-  event.preventDefault()
-  console.log('deck')
-  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-  .then(res =>  res.json())
-  .then(data => {
-    document.querySelector('#player1').src = data.cards[0].image;
-    document.querySelector('#player2').src = data.cards[1].image;
+async function getDeckID() {
+  try {
+    let res = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+    let data = await res.json();
+    return data.deck_id;
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+}
 
-    let player1card = convertToNum(data.cards[0].value);
-    let player2card = convertToNum(data.cards[1].value);
+// Calling the function with async/await
+async function getDeck() {
+  let id = await getDeckID();
+  try {
+    let res = await fetch(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`);
+    let data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+}
+
+getDeck(); 
+
+async function drawTwo(event){
+  event.preventDefault();
+  let deck = await getDeck();
+  console.log(deck.cards);
+    document.querySelector('#player1').src = deck.cards[0].image;
+    document.querySelector('#player2').src = deck.cards[1].image;
+
+    console.log(deck.cards[0].value > deck.cards[1].value);
+    
+
+    let player1card = convertToNum(deck.cards[0].value);
+    let player2card = convertToNum(deck.cards[1].value);
 
     if(player1card > player2card){
       console.log('Player One Wins!');
@@ -36,10 +45,6 @@ function drawTwo(event){
     } else {
       console.log('This is War!');
     }
-  })
-  .catch((err) => {
-    console.log(`error ${err}` )
-  })
 }
 
 function convertToNum(val){
